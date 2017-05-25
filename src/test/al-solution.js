fe00eea1f3425fe6dd2b9afe42c842e5015a4663
Aileen Santos <asantos3026@gmail.com>
2017-05-25 12:03:34 -0700

    WIP connected mocha to assignment

--- src/test/al-solution.js
+++ src/test/al-solution.js
@@ -1,14 +1,41 @@
 import chai, { expect } from 'chai'
 import chaiChange from 'chai-change'
+import template from '..'
 
 chai.use(chaiChange)
 
 describe('template', () => {
 
-  it('returns html markup', () => {
+  it('returns an array with the same number of outputs as inputs', () => {
+    const input = {
+      "members": [
+        {
+          "id": 1,
+          "email": "jill@mycompany.com",
+          "name": "Jill",
+          "team": "engineering"
+        },
+        {
+          "id": 2,
+          "email": "rohit@mycompany.com",
+          "name": "Rohit",
+          "team": "finance"
+        },
+        {
+          "id": 3,
+          "email": "maria@mycompany.com",
+          "name": "Maria",
+          "team": "operations"
+        }
+      ],
+      "restaurant": {
+          "logo": "https://vignette3.wikia.nocookie.net/logopedia/images/3/3f/Pizza_Hut_logo_old.svg/revision/latest/scale-to-width-down/200?cb=20121206223303",
+          "name": "Pizza R Us",
+          "yelp_link": "https://yelp.com/pizza-r-us"
+      }
+    }
 
-
-    expect(bst.root).to.eql({value: 3, left: {value:2, left: null, right: null}, right: {value:5, left: null, right: null} })
+    expect(template(input).length).to.eql(3)
   })
 
 })
